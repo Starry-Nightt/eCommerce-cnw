90033b71b221b5a5a59c2e09213f5646a088152b
dangminhtien051202@gmail.com <dangminhtien051202@gmail.com>
2023-10-20 13:55:03 +0700

    up interceptor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,9 @@ import "../styles/globals.css";
 import Providers from "@/redux/providers";
 import LayoutDefault from "@/layouts/default/layout-default";
 import { useEffect } from "react";
-import createLoadingInterceptor from "shared/interceptors/loading.interceptor";
 import { store } from "@/redux/store";
 import LayoutAdmin from "@/layouts/admin/layout-admin";
+import createLoadingInterceptor from "@/interceptors/loading.interceptor";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
